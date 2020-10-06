23c5dff73358a0574120337bbc25f4ea11927e2b
Stuzerain <shanford3@gmail.com>
2020-10-06 16:38:33 -0700

    Improve file structure/organization

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['Services/Add-To-Cart/Dist/bundle.js', 'Services/ImageCarousel/public/bundle.js', 'Services/Reviews/public/bundle.js', 'Services/Seller/client/dist/bundle.js'],
+        dest: 'bundles/built.js',
+      },
+    },
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'bundles/built.min.js': ['bundles/built.js']
+        }
+      }
+    },
+    cssmin: {
+      options: {
+        mergeIntoShorthands: false,
+        roundingPrecision: -1
+      },
+      target: {
+        files: {
+          'bundles/compiled.css': ['public/proxy.css', 'Services/ImageCarousel/public/style.css', 'Services/Reviews/public/style.css', 'Services/Seller/client/dist/style.css']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Stu-Proxy
+
+This repo assumes that you have the repos for each of the Petsy services available within the 'Services' folder of this repo, like so:
+
+Stu-proxy
+  -Services
+  ---Add-To-Cart
+  ---ImageCarousel
+  ---Reviews
+  ---Seller
