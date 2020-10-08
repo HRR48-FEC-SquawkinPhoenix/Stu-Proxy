9aaa795b3b307fbb53ca51c4e160d46151a17b68
Stuzerain <shanford3@gmail.com>
2020-10-08 11:41:33 -0700

    Add bottom border to header, import proxy.css into index.html

--- server/template.js
+++ server/template.js
@@ -6,12 +6,12 @@ var component = `
   <title>Petsy Proxy</title>
   <!-- <link rel="stylesheet" type="text/css" href="http://localhost:3001/style.css">
   <link rel="stylesheet" type="text/css" href="http://localhost:3002/style.css">
-  <link rel="stylesheet" type="text/css" href="http://localhost:3004/style.css">
-  <link rel="stylesheet" type="text/css" href="proxy.css"> -->
-  <link rel="stylesheet" type="text/css" href="https://fec-built.s3.us-east-2.amazonaws.com/compiled.css">
+  <link rel="stylesheet" type="text/css" href="http://localhost:3004/style.css"> -->
 
   <!-- <link rel="stylesheet" type="text/css" href="compiled.css"> -->
 
+  <link rel="stylesheet" type="text/css" href="https://fec-built.s3.us-east-2.amazonaws.com/compiled.css">
+  <link rel="stylesheet" type="text/css" href="proxy.css">
 </head>
 
 <body>
@@ -26,14 +26,14 @@ var component = `
     <div id="seller"></div>
   </div>
 
-
   <!-- <script src="http://localhost:3001/bundle.js"></script>
-  <script src="http://localhost:3002/bundle.js"></script>
-  <script src="http://localhost:3003/bundle.js"></script>
-  <script src="http://localhost:3004/bundle.js"></script> -->
+    <script src="http://localhost:3002/bundle.js"></script>
+    <script src="http://localhost:3003/bundle.js"></script>
+    <script src="http://localhost:3004/bundle.js"></script> -->
+
+  <!-- <script src="built.js"></script> -->
 
   <script src="https://fec-built.s3.us-east-2.amazonaws.com/built2.js"></script>
-  <!-- <script src="built.js"></script> -->
 </body>
 
 </html>
